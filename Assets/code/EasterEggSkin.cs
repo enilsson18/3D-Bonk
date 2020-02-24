82e42f023a51a43388b15334435d4f4aef0e036a
BuildTools <unconfigured@null.spigotmc.org>
2020-02-23 20:01:24 -0500

    auto commit

---
+++ Assets/code/EasterEggSkin.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EasterEggSkin : MonoBehaviour
+{
+    public Material material;
+
+    private void OnTriggerEnter(Collider collision)
+    {
+        collision.transform.gameObject.GetComponent<MeshRenderer>().material = material;
+        print("changed skin");
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
