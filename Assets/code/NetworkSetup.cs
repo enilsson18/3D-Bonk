e2168a35ed8aacc9d003c1b43860fe4795af3c2c
BuildTools <unconfigured@null.spigotmc.org>
2020-04-05 12:49:27 -0400

    update ip and mirror versions

---
+++ Assets/code/NetworkSetup.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class NetworkSetup : MonoBehaviour
+{
+    private string ip;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ip = IPManager.GetIP(ADDRESSFAM.IPv4);
+        gameObject.GetComponent<NetworkManager>().networkAddress = ip;
+
+        print(ip);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
